e7323d36211bcaf704792b66bcb1162a529fb2d9
Nicopelullo <82542233+Nicopelullo@users.noreply.github.com>
2021-05-06 16:58:55 -0300

    Add files via upload

---
+++ 01-Assert/TipodeDatos.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cassert>
+
+int main() {
+	
+	char letra= 'B';
+	
+	assert(letra !='C');
+	assert(true&true==true);
+	assert(245.2!= 240.12);
+	assert(-2+3==1);
+	assert(12+13==25);
+
+
+
+
+}
